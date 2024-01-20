3e0d29bade5d09e32de7c6349c5d63244213bc6a
Diwan Malla <diwanmalla@Diwans-MacBook-Air.local>
2024-01-21 00:22:31 +1100

    Added date correctly

--- src/components/part 3/index.tsx
+++ src/components/part 3/index.tsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
-const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+const weekdays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 const PartThree = () => {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
 
-  const date = new Date();
-  const dayIndex = (date.getDay() + count) % 7;
+  const getCurrentDate = () => {
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + count);
+    return currentDate;
+  };
+
+  const dayIndex = getCurrentDate().getDay() % 7;
   const day = weekdays[dayIndex];
-  const fullDate = date.toLocaleDateString();
+
+  const month = getCurrentDate().toLocaleString("default", { month: "short" });
+  const date = getCurrentDate().getDate();
+  const year = getCurrentDate().getFullYear();
   const handleSubtractStep = () => {
     if (step > 1) setStep(step - 1);
   };
@@ -67,11 +67,11 @@ const PartThree = () => {
           </button>
         </div>
         <div>
-          {count == 0 && `Today is ${weekdays[date.getDay()]} ${fullDate}`}
+          {count == 0 && `Today is ${day} ${month} ${date} ${year}`}
           {count != 0 && (
             <div id="message">{`${count > 0 ? count : -count}${
               count > 0 ? " days from today" : " days ago"
-            } days is ${day}`}</div>
+            } days is ${day} ${month} ${date} ${year}`}</div>
           )}
         </div>
       </div>
